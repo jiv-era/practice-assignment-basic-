1ea77ed8c729d234a2f9d451d6d79f1619791391
Jordan R <jaygerzero@gmail.com>
2020-03-08 17:27:32 -0400

    components set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 import './App.css';
 
 class App extends Component {
+  state = {
+  };
+
   render() {
     return (
       <div className="App">
